ffb12c6d4fa3dd94dcd23ee09516daa8157ab938
prataphal <30648677+prataphal@users.noreply.github.com>
2017-08-08 10:14:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/aritcle-one', function (req, res) {
+  res.send('aritcle one published');
+});
+
+app.get('/aritcle-two', function (req, res) {
+  res.send('aritcle one published');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
