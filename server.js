de76fb3f9ea4add4d416704ea0a9ba693b8432d2
prataphal <30648677+prataphal@users.noreply.github.com>
2017-08-11 16:46:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-var count=0;
+var counter=0;
 app.get('/counter', function (req, res) {
     counter=counter+1;
   res.send(counter.toString());
